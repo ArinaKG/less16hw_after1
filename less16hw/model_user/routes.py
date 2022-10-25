7a3966f156a780372fb4d9d6e5c0fd0a0a9014a9
Арина <a.kornishina@mail.ru>
2022-10-25 17:31:59 +0300

    try3

---
+++ less16hw/model_user/routes.py
@@ -0,0 +1,68 @@
+import json, db
+from app import models
+from flask import current_app as app, request, jsonify
+
+@app.route('/', methods=['GET', 'POST'])
+def users():
+    if request.method == "GET":
+        result = []
+        for user in models.User.query.all():
+            result.append(user.to_dict())
+        return "Пользователь создан"
+    elif request.method == "POST":
+        user_data = request.json
+
+        new_user = models.User(**user_data)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        for user in models.User.query.all():
+            result.append(user.to_dict())
+
+        return jsonify(result), 200
+
+
+@app.route('/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
+def user_function(uid):
+    if request.method == "GET":
+        user = models.User.query.get(uid)
+        return jsonify(user.to_dict()), 200
+    if request.method == "PUT":
+        user_data = request.json
+        user = models.User.query.get(uid)
+        user.first_name = user_data['first_name']
+        user.last_name = user_data['last_name']
+        user.age = user_data['age']
+        user.email = user_data['email']
+        user.role = user_data['role']
+        user.phone = user_data['phone']
+
+        db.session.add(user)
+        db.session.commit()
+
+        user = models.User.query.get(uid)
+        return "Пользователь обновлён"
+
+    if request.method == "DELETE":
+        user = models.User.query.get(uid)
+        db.session.delete(user)
+        db.session.commit()
+
+        return "Пользователь удален"
+
+
+@app.route('/order/<int:id_order>', methods=['GET', 'PUT', 'DELETE'])
+def page_order_by_id(id_order):
+    if request.method == "GET":
+        res = Utils()
+        return jsonify(res.get_by_id(Order, id_order))
+    elif request.method == 'PUT':
+        content = request.json
+        Utils().update_data_order(Order, content, id_order)
+        return "Заказ обновлён"
+    else:
+        Utils().delete_data_in_bd(Order, id_order)
+        return "Заказ удален"
+
+
